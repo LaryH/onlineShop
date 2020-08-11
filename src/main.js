a79690967eeca8449e1efe1baee5682e2e0aeb72
Lassell <Lassell@qq.com>
2020-08-11 10:44:32 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import Vue from "vue";
+import App from "./App";
+
+Vue.config.productionTip = false;
+
+//组件3大步:定义.注册.使用
+
+new Vue({
+  // el: "#app",v
+  render: (h) => h(App), //  1.注册组件  2.使用组件  3.渲染组件
+  // components: {
+  //   App,
+  // },
+  // template: "<App />",
+}).$mount("#app");
---
+++ vue.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  resovle: {
+    alias: {
+      vue$: "vue/dis/vue.js",
+    },
+  },
+};
